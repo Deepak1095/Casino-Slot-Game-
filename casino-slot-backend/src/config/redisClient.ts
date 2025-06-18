a3e5ed8e4510bcd171712ac4b9d4e84991ff3882
deepak <ddhankhar999@gmail.com>
2025-06-18 17:56:46 +0530

    update redis config

--- casino-slot-backend/src/config/redisClient.ts
+++ casino-slot-backend/src/config/redisClient.ts
@@ -1,8 +1,9 @@
 import Redis from 'ioredis';
 
-// console.log(process.env.REDIS_URL!,process.env.REDIS_TOKEN)
-const redis = new Redis(process.env.REDIS_URL!, {
-  password: process.env.REDIS_TOKEN,
-});
+// const redis = new Redis(process.env.REDIS_URL!, {
+//   password: process.env.REDIS_TOKEN,
+// });
+
+const redis = new Redis('rediss://:AYsSAAIjcDFlZGNmYzUxMDI3YmI0OTMwODcwNDkyY2M1OTBjYmUxN3AxMA@ace-dory-35602.upstash.io:6379');
 
 export default redis;
