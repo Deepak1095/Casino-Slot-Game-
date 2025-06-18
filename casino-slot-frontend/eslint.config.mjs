02a809c8d273fea0d1dd0d898e235889bbe28a64
deepak <ddhankhar999@gmail.com>
2025-06-18 21:14:55 +0530

    eslint no-explicit-any:off

--- casino-slot-frontend/eslint.config.mjs
+++ casino-slot-frontend/eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Add custom rule overrides here
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
